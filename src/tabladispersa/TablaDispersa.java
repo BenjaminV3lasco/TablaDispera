e735945a80200d4725858741ecc31599f0a4d04e
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-05-23 13:10:47 -0300

    Version 1

---
+++ src/tabladispersa/TablaDispersa.java
@@ -0,0 +1,14 @@
+
+package tabladispersa;
+import java.io.*;
+public class TablaDispersa {
+    
+
+    public static void main(String[] args) {
+        StringBuffer poblacion, direccion;
+        int numhabitacion;
+        double precio;
+        
+    }
+    
+}
