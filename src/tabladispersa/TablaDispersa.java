b5a57ad5a8e3b2c5a2ad2e6d8afd7803926645bc
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-05-23 14:08:48 -0300

    Mejoras

--- src/tabladispersa/TablaDispersa.java
+++ src/tabladispersa/TablaDispersa.java
@@ -1,15 +1,33 @@
 
 package tabladispersa;
-import java.io.*;
+import java.util.Scanner;
 public class TablaDispersa {
     
 
     public static void main(String[] args) {
         StringBuffer poblacion, direccion;
         String clave;
-        int numhabitacion;
+        int numhabitacion,respuesta;
+        respuesta=0;
+        Scanner Teclado = new Scanner(System.in);
         double precio;
-        
+        do{
+            System.out.println("¡Buenos dias!");
+            System.out.println("Ingrese 1 para insertar una casa");
+            System.out.println("Ingrese 2 para eliminar una casa");
+            System.out.println("Ingrese 3 para salir");
+            respuesta=Teclado.nextInt();
+                while(respuesta<1 || respuesta>3){
+                System.out.println("Error: La opcion ingresa no es válida");
+                System.out.println("Ingrese 1 para insertar una casa");
+                System.out.println("Ingrese 2 para eliminar una casa");
+                System.out.println("Ingrese 3 para salir");
+                respuesta=Teclado.nextInt();
+                }
+            switch(respuesta){
+                
+            }    
+        }while(respuesta<3);
     }
     
 }
