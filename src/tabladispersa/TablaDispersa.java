074f82540af522d32a66a797f3bf2879c2715602
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-05-23 13:36:36 -0300

    Mejoras

--- src/tabladispersa/TablaDispersa.java
+++ src/tabladispersa/TablaDispersa.java
@@ -6,6 +6,7 @@ public class TablaDispersa {
 
     public static void main(String[] args) {
         StringBuffer poblacion, direccion;
+        String clave;
         int numhabitacion;
         double precio;
         
